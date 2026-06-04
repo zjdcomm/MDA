f5cbf0863e5b20a6c7e1f9bc31af6e909c308c4c
zjdcomm <84618212+zjdcomm@users.noreply.github.com>
2026-06-04 21:24:09 +0800

    feat: Remove membership module registration

--- agent/go-service/register.go
+++ agent/go-service/register.go
@@ -4,7 +4,6 @@ import (
 	"github.com/1204244136/MDA/agent/go-service/pkg/resource"
 	"github.com/1204244136/MDA/agent/go-service/taskersink/aspectratio"
 	"github.com/1204244136/MDA/agent/go-service/taskersink/hdrcheck"
-	"github.com/1204244136/MDA/agent/go-service/taskersink/membership"
 	"github.com/1204244136/MDA/agent/go-service/taskersink/processcheck"
 	"github.com/rs/zerolog/log"
 )
@@ -17,7 +16,7 @@ func registerAll() {
 	aspectratio.Register()
 	hdrcheck.Register()
 	processcheck.Register()
-	membership.Register()
+	// membership.Register() - DISABLED: Membership system removed
 
 	log.Info().
 		Msg("All custom components and sinks registered successfully")
